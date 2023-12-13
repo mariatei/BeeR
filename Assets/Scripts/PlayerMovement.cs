91ed2b3e41c2fbc73a83ded5dd0cd81d13afdb1f
Ma-tee-o <matteoippolito88@gmail.com>
2023-12-13 17:53:41 +0100

    Resetet zum Stand "Neue Assets und Basic Movement" vom 23. November

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,26 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5.0f; // Geschwindigkeit der Bewegung des Spielers
-    public float rotationSpeed = 3.0f; // Geschwindigkeit der Rotation des Spielers
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
     void Update()
     {
-        // Eingaben von den Pfeiltasten
-        float horizontalMove = Input.GetAxis("Horizontal"); // Pfeiltasten links/rechts
-        float verticalMove = Input.GetAxis("Vertical"); // Pfeiltasten hoch/runter
-
-        // Bewegung des Spielers basierend auf den Pfeiltasten
-        Vector3 moveDirection = new Vector3(horizontalMove, 0.0f, verticalMove) * moveSpeed * Time.deltaTime;
-        transform.Translate(moveDirection, Space.Self);
-
-        // Rotation des Spielers basierend auf den Pfeiltasten
-        if (horizontalMove != 0 || verticalMove != 0)
-        {
-            float targetAngle = Mathf.Atan2(horizontalMove, verticalMove) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
-        }
+        
     }
 }
-
